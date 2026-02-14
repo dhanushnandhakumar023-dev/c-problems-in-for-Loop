b4b10a409805d9986fe0bdbed75e28ef3cc7d6d5
dhanushnandhakumar023-dev <dhanushnandhakumar023@gmail.com>
2026-02-14 10:06:09 +0530

    Create Fibonacci Series.c

---
+++ Fibonacci Series.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+    int a=0,b=1,next;
+    scanf("%d", &n);
+    for(int i=1;i<=n;i++) {
+        printf("%d ",a);
+        next = a+b;
+        a=b;
+        b=next;
+    }
+    return 0;
+}
