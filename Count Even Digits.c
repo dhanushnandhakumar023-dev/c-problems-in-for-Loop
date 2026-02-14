6e77af13252db1de8ea1dc9256d21438f4d111e7
dhanushnandhakumar023-dev <dhanushnandhakumar023@gmail.com>
2026-02-14 13:56:09 +0530

    Create Count Even Digits.c

---
+++ Count Even Digits.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    int count=0;
+    scanf("%d",&n);
+    for(int i=n;i>0;i=i/10)
+    {
+        int digit=i %10;
+        if(digit % 2==0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+    return 0;
+}
